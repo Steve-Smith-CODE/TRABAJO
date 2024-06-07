e6f8492f64a705b596526f3eff6b393d31c8ab68
Steve-Smith-CODE <steven.ovallecito@gmail.com>
2024-06-06 22:42:26 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,341 @@
+﻿namespace TRABAJO
+{
+    partial class form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtNombres = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDocumento = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboTipo = new System.Windows.Forms.ComboBox();
+            this.btnImprimir = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.dgvCurso = new System.Windows.Forms.DataGridView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCurso = new System.Windows.Forms.TextBox();
+            this.txtCredito = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCurso)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.groupBox1.Controls.Add(this.txtNombres);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtDocumento);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Modern No. 20", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(54, 59);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(831, 83);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Detalles del Estudiante";
+            // 
+            // txtNombres
+            // 
+            this.txtNombres.Location = new System.Drawing.Point(484, 24);
+            this.txtNombres.Multiline = true;
+            this.txtNombres.Name = "txtNombres";
+            this.txtNombres.Size = new System.Drawing.Size(199, 28);
+            this.txtNombres.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(301, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(169, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Apellido y Nombres:";
+            // 
+            // txtDocumento
+            // 
+            this.txtDocumento.Location = new System.Drawing.Point(154, 25);
+            this.txtDocumento.Multiline = true;
+            this.txtDocumento.Name = "txtDocumento";
+            this.txtDocumento.Size = new System.Drawing.Size(121, 28);
+            this.txtDocumento.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nro. Documento:";
+            // 
+            // cboTipo
+            // 
+            this.cboTipo.FormattingEnabled = true;
+            this.cboTipo.Location = new System.Drawing.Point(590, 16);
+            this.cboTipo.Name = "cboTipo";
+            this.cboTipo.Size = new System.Drawing.Size(121, 28);
+            this.cboTipo.TabIndex = 8;
+            // 
+            // btnImprimir
+            // 
+            this.btnImprimir.BackColor = System.Drawing.Color.PaleGreen;
+            this.btnImprimir.Font = new System.Drawing.Font("Modern No. 20", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnImprimir.Location = new System.Drawing.Point(577, 237);
+            this.btnImprimir.Name = "btnImprimir";
+            this.btnImprimir.Size = new System.Drawing.Size(308, 34);
+            this.btnImprimir.TabIndex = 9;
+            this.btnImprimir.Text = "Imprimir";
+            this.btnImprimir.UseVisualStyleBackColor = false;
+            this.btnImprimir.Click += new System.EventHandler(this.btnImprimir_Click);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.BackColor = System.Drawing.Color.PaleGreen;
+            this.btnAgregar.Font = new System.Drawing.Font("Modern No. 20", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregar.Location = new System.Drawing.Point(54, 238);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(242, 34);
+            this.btnAgregar.TabIndex = 8;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = false;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // dgvCurso
+            // 
+            this.dgvCurso.BackgroundColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.dgvCurso.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCurso.Location = new System.Drawing.Point(54, 278);
+            this.dgvCurso.Name = "dgvCurso";
+            this.dgvCurso.RowHeadersWidth = 51;
+            this.dgvCurso.RowTemplate.Height = 24;
+            this.dgvCurso.Size = new System.Drawing.Size(831, 205);
+            this.dgvCurso.TabIndex = 7;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.groupBox2.Controls.Add(this.txtCredito);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.cboTipo);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.txtPrecio);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.txtCantidad);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.txtCurso);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Font = new System.Drawing.Font("Modern No. 20", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(54, 148);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(831, 85);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Detalles del Curso";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(464, 16);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(111, 20);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Tipo de Pago";
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(77, 54);
+            this.txtPrecio.Multiline = true;
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(154, 28);
+            this.txtPrecio.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(492, 54);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 20);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Cantidad:";
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(590, 55);
+            this.txtCantidad.Multiline = true;
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(125, 28);
+            this.txtCantidad.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Precio:";
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.Color.PaleGreen;
+            this.btnSalir.Font = new System.Drawing.Font("Modern No. 20", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.ForeColor = System.Drawing.Color.Black;
+            this.btnSalir.Location = new System.Drawing.Point(664, 487);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(221, 34);
+            this.btnSalir.TabIndex = 10;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.BackColor = System.Drawing.Color.PaleGreen;
+            this.btnLimpiar.Font = new System.Drawing.Font("Modern No. 20", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.Location = new System.Drawing.Point(334, 239);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(263, 34);
+            this.btnLimpiar.TabIndex = 11;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = false;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.White;
+            this.label7.Enabled = false;
+            this.label7.Font = new System.Drawing.Font("Stencil", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.Red;
+            this.label7.Location = new System.Drawing.Point(136, 18);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(502, 27);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Gestor de Matrículas Universitarias";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 20);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Curso:";
+            // 
+            // txtCurso
+            // 
+            this.txtCurso.Location = new System.Drawing.Point(77, 22);
+            this.txtCurso.Multiline = true;
+            this.txtCurso.Name = "txtCurso";
+            this.txtCurso.Size = new System.Drawing.Size(154, 28);
+            this.txtCurso.TabIndex = 1;
+            // 
+            // txtCredito
+            // 
+            this.txtCredito.Location = new System.Drawing.Point(346, 46);
+            this.txtCredito.Multiline = true;
+            this.txtCredito.Name = "txtCredito";
+            this.txtCredito.Size = new System.Drawing.Size(112, 28);
+            this.txtCredito.TabIndex = 11;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(263, 49);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(77, 20);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Creditos:";
+            // 
+            // form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.ClientSize = new System.Drawing.Size(943, 533);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnImprimir);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.dgvCurso);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PROYECTO";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCurso)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtNombres;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDocumento;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboTipo;
+        private System.Windows.Forms.Button btnImprimir;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.DataGridView dgvCurso;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtCurso;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCredito;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.tool.xml;
+
+namespace TRABAJO
+{
+    public partial class form1 : Form
+    {
+        private bool columnasAgregadas = false;
+
+        public form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cboTipo.Items.Add("Contado");
+            cboTipo.Items.Add("Tarjeta");
+            cboTipo.SelectedIndex = 0;
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+{   
+    if (!columnasAgregadas)
+    {
+        dgvCurso.Columns.Add("Cantidad", "Cantidad");
+        dgvCurso.Columns.Add("Descripción", "Descripción");
+        dgvCurso.Columns.Add("PrecioUnitario", "Precio Unitario");
+        dgvCurso.Columns.Add("Descuento", "Descuento");
+        dgvCurso.Columns.Add("Importe", "Importe");
+        dgvCurso.Columns.Add("Créditos", "Créditos");
+        columnasAgregadas = true;
+        dgvCurso.AllowUserToAddRows = false;
+    }
+
+    try
+    {
+        string tipo = cboTipo.Text;
+        double subtotal = double.Parse(txtCantidad.Text) * double.Parse(txtPrecio.Text);
+        double descuento = tipo.Equals("Contado") ? 0.05 * subtotal : 0;
+        int indice_fila = dgvCurso.Rows.Add();
+        DataGridViewRow fila = dgvCurso.Rows[indice_fila];
+        fila.Cells["Cantidad"].Value = txtCantidad.Text;
+        fila.Cells["Descripción"].Value = txtCurso.Text;
+        fila.Cells["PrecioUnitario"].Value = txtPrecio.Text;
+        fila.Cells["Descuento"].Value = descuento.ToString("0.00");
+        fila.Cells["Importe"].Value = (subtotal - descuento).ToString("0.00");
+        fila.Cells["Créditos"].Value = txtCredito.Text;
+    }
+    catch (Exception ex)
+    {
+        MessageBox.Show("Error al agregar fila: " + ex.Message);
+    }
+}
+
+        private void btnImprimir_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog savefile = new SaveFileDialog();
+            savefile.FileName = $"Reporte_{DateTime.Now:dd_MM_yyyy_HH_mm_ss}" + ".pdf";
+
+            string PaginaHTML_Texto = Properties.Resources.plantilla;
+
+            if (string.IsNullOrEmpty(txtNombres.Text) || string.IsNullOrEmpty(txtDocumento.Text))
+            {
+                MessageBox.Show("Por favor complete todos los campos.");
+                return;
+            }
+
+            PaginaHTML_Texto = PaginaHTML_Texto.Replace("@ESTUDIANTE", txtNombres.Text);
+            PaginaHTML_Texto = PaginaHTML_Texto.Replace("@DOCUMENTO", txtDocumento.Text);
+            PaginaHTML_Texto = PaginaHTML_Texto.Replace("@FECHA", DateTime.Now.ToString("dd/MM/yyyy"));
+
+            string filas = string.Empty;
+            decimal total = 0;
+            foreach (DataGridViewRow row in dgvCurso.Rows)
+            {
+                if (row.Cells["Cantidad"].Value != null &&
+                    row.Cells["Descripción"].Value != null &&
+                    row.Cells["PrecioUnitario"].Value != null &&
+                    row.Cells["Importe"].Value != null)
+                {
+                    filas += "<tr>";
+                    filas += "<td>" + row.Cells["Cantidad"].Value.ToString() + "</td>";
+                    filas += "<td>" + row.Cells["Descripción"].Value.ToString() + "</td>";
+                    filas += "<td>" + row.Cells["PrecioUnitario"].Value.ToString() + "</td>";
+                    filas += "<td>" + row.Cells["Importe"].Value.ToString() + "</td>";
+                    filas += "<td>" + row.Cells["Créditos"].Value.ToString() + "</td>";
+                    filas += "</tr>";
+                    total += decimal.Parse(row.Cells["Importe"].Value.ToString());
+                }
+                else
+                {
+                    MessageBox.Show("Asegúrese de que todas las filas tengan valores completos.");
+                    return;
+                }
+            }
+            PaginaHTML_Texto = PaginaHTML_Texto.Replace("@FILAS", filas);
+            PaginaHTML_Texto = PaginaHTML_Texto.Replace("@TOTAL_MATRICULA", total.ToString("0.00"));
+
+            if (savefile.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = savefile.FileName;
+                using (FileStream stream = new FileStream(filePath, FileMode.Create))
+                {
+                    Document pdfDoc = new Document(PageSize.A4, 25, 25, 25, 25);
+                    PdfWriter writer = PdfWriter.GetInstance(pdfDoc, stream);
+                    pdfDoc.Open();
+                    pdfDoc.Add(new Phrase(""));
+
+                    try
+                    {
+                        iTextSharp.text.Image img = iTextSharp.text.Image.GetInstance(Properties.Resources.images, System.Drawing.Imaging.ImageFormat.Png);
+                        img.ScaleToFit(80, 80);
+                        img.Alignment = iTextSharp.text.Image.UNDERLYING;
+                        img.SetAbsolutePosition(pdfDoc.LeftMargin, pdfDoc.Top - 60);
+                        pdfDoc.Add(img);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error al agregar la imagen: " + ex.Message);
+                    }
+
+                    using (StringReader sr = new StringReader(PaginaHTML_Texto))
+                    {
+                        XMLWorkerHelper.GetInstance().ParseXHtml(writer, pdfDoc, sr);
+                    }
+
+                    pdfDoc.Close();
+                    stream.Close();
+                }
+            }
+        }
+
+
+
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtCantidad.Text = "";
+            txtCurso.Text = "";
+            txtPrecio.Text = "";
+            cboTipo.SelectedIndex = 0;
+        }
+    }
+}
